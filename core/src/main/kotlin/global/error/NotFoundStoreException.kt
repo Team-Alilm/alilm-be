6232be9c650d559250398632659d8d79c2c41c30
jangjuyeong.jang <jooyoung.jang@skylabs.io>
2025-05-23 09:00:31 +0900

    feat

--- core/src/main/kotlin/global/error/NotFoundStoreException.kt
+++
@@ -1,7 +0,0 @@
-package org.team_alilm.global.error
-
-class NotFoundStoreException : RuntimeException() {
-
-    override val message: String
-        get() { return "${Error.NOT_FOUND_STORE.code} : ${Error.NOT_FOUND_STORE.message}" }
-}
