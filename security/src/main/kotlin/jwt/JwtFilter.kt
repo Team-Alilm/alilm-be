b091c42923a09f7cc8fc6f4989e6a09241a85c41
jangjuyeong.jang <jooyoung.jang@skylabs.io>
2025-05-26 15:46:27 +0900

    feat

--- security/src/main/kotlin/jwt/JwtFilter.kt
+++ security/src/main/kotlin/jwt/JwtFilter.kt
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.web.filter.OncePerRequestFilter
 import org.team_alilm.global.error.NotFoundMemberException
 import org.team_alilm.service.CustomUserDetailsService
